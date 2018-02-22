622eca33f08a68fb58745498f87674e1955ba6d5
B Terbush <brittanyterbush@gmail.com>
2018-02-21 16:29:59 -0800

    declare function to display all Pokemon in Mutably api database

--- public/script.js
+++ public/script.js
@@ -2,6 +2,31 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+  function allPokes() {
+  fetch('http://mutably.herokuapp.com/pokemon')
+    .then(response =>
+      response.json()
+    ).then((res) => {
+      console.log();
+      const pokeNodes = []
+      res.pokemon.forEach((pokemon) => {
+        pokeNodes.push($(`
+          <li><img src="${pokemon.image}"></li>
+          <li>${pokemon.name}</li>
+          <button class='get-book' id=${pokemon._id}>Details</button>
+          <button class='delete-book' id=D${pokemon._id}>Delete</button>`))
+      })
+      console.log(pokeNodes);
+      $('.list-group').empty()
+      pokeNodes.forEach((node) => {
+        $('.list-group').append(node)
+      })
+      // $('.get-poke').on('click', getPoke)
+      // $('.delete-poke').on('click', deletePoke)
+    })
+}
+
+  allPokes();
+
 
 });
